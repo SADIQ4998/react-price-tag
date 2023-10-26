59234b2676ff4df4c5a38fce9d7c59894a5151fe
SADIQ4998 <137378282+SADIQ4998@users.noreply.github.com>
2023-10-26 15:16:35 +0530

    Add files via upload

---
+++ App.1.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+export function App() {
+    return (
+        <><div className="full"></div><><><div className="myDiv">
+            <h3>FREE</h3>
+            <h2>$0/month</h2>
+
+            <ol>
+                <li>Single User</li>
+                <li>50GB Storage</li>
+                <li>Unlimited Public Project</li>
+                <li>Community Access</li>
+                <li>Unlimited Private Project</li>
+                <li>Dedicated Phone Support</li>
+                <li>Free Subdomain</li>
+                <li>Monthly Status Reports</li>
+                <button>button</button>
+            </ol>
+        </div>
+            <div className="plus">
+                <h3>PLUS</h3>
+                <h2>$9/month</h2>
+                <ol>
+                    <li>Single User</li>
+                    <li>50GB Storage</li>
+                    <li>Unlimited Public Project</li>
+                    <li>Community Access</li>
+                    <li>Unlimited Private Project</li>
+                    <li>Dedicated Phone Support</li>
+                    <li>Free Subdomain</li>
+                    <li>Monthly Status Reports</li>
+                    <button>button</button>
+                </ol>
+            </div></>
+            <div className="PRO">
+                <h3>PRO</h3>
+                <h2>$49/month</h2>
+                <ol>
+                    <li>Single User</li>
+                    <li>50GB Storage</li>
+                    <li>Unlimited Public Project</li>
+                    <li>Community Access</li>
+                    <li>Unlimited Private Project</li>
+                    <li>Dedicated Phone Support</li>
+                    <li>Free Subdomain</li>
+                    <li>Monthly Status Reports</li>
+                    <button>button</button>
+                </ol>
+
+            </div></></>
+    );
+}
